ab23acccd0babe03303387dca08d5de3eaefe137
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2021-12-07 16:18:32 +0100

    split GENERATE_TESTSUITE to separate script

---
+++ GENERATE_TESTSUITE.m
@@ -0,0 +1,103 @@
+%% Init
+clear;
+clc;
+addpath('data');
+addpath('functions');
+rng(1);
+
+%% Source test generation
+disp('Generating source test cases...')
+nTest = 100; %Number of test cases to generate
+sourceTestSuite = generateTestCasesRandomly(nTest);
+
+%% Followup test generation
+disp('Generating follow-up test cases...')
+MRIP={@(a) generateFollowUpMRIP1_1(a),@(a) generateFollowUpMRIP1_2(a),@(a) generateFollowUpMRIP1_3(a),@(a) generateFollowUpMRIP2(a),@(a) generateFollowUpMRIP3(a),@(a) generateFollowUpMRIP4(a)};
+MRIP_Count = size(MRIP,2);
+Mutants=["purePursuitUSCityCopia","Mutant_1_of_purePursuitUSCityCopia","Mutant_2_of_purePursuitUSCityCopia","Mutant_3_of_purePursuitUSCityCopia","Mutant_4_of_purePursuitUSCityCopia","Mutant_5_of_purePursuitUSCityCopia","Mutant_6_of_purePursuitUSCityCopia","Mutant_7_of_purePursuitUSCityCopia","Mutant_8_of_purePursuitUSCityCopia","Mutant_9_of_purePursuitUSCityCopia","Mutant_10_of_purePursuitUSCityCopia",...
+    "Mutant_11_of_purePursuitUSCityCopia","Mutant_12_of_purePursuitUSCityCopia","Mutant_13_of_purePursuitUSCityCopia","Mutant_14_of_purePursuitUSCityCopia","Mutant_15_of_purePursuitUSCityCopia","Mutant_16_of_purePursuitUSCityCopia","Mutant_17_of_purePursuitUSCityCopia","Mutant_18_of_purePursuitUSCityCopia","Mutant_19_of_purePursuitUSCityCopia","Mutant_20_of_purePursuitUSCityCopia"];
+Mutants_Count = size(Mutants,2);
+
+followupTestSuite(Mutants_Count,MRIP_Count,nTest) = sourceTestSuite{1,1};
+
+for mutant=0:Mutants_Count-1
+    for mrip=0:MRIP_Count-2
+        for test=1:nTest
+            followupTestSuite(mutant+1,mrip+1,test) = MRIP{mrip+1}(sourceTestSuite{test});
+        end
+    end
+end
+
+% MRIP4 was generated last, and needs to be kept that way for RNG
+% compatibility with original experiments
+mrip = MRIP_Count-1;
+for mutant=0:19
+    for test=1:nTest
+        followupTestSuite(mutant+1,mrip+1,test) = MRIP{mrip+1}(sourceTestSuite{test});
+    end
+end
+
+%% Save testSuite
+disp('Saving test suites...')
+save('testSuite.mat', 'sourceTestSuite', 'followupTestSuite');
+
+%% Follow-up test generation functions
+
+function testCase = generateFollowUpMRIP1_1(sourceTestCase)
+    %MRIP: increment nominal speed
+    testCase = sourceTestCase;
+    testCase.nominalSpeed = testCase.nominalSpeed *1.1;
+    
+end
+
+function testCase = generateFollowUpMRIP1_2(sourceTestCase)
+    %MRIP: increment nominal speed
+    testCase = sourceTestCase;
+    testCase.nominalSpeed = testCase.nominalSpeed *1.2;
+end
+function testCase = generateFollowUpMRIP1_3(sourceTestCase)
+    %MRIP: increment nominal speed
+    testCase = sourceTestCase;
+    testCase.nominalSpeed = testCase.nominalSpeed *1.3;
+end
+
+function testCase = generateFollowUpMRIP2(sourceTestCase)
+    %MRIP: Generate vehicles inside of the vehicle's path
+    generateVehiclesMiddle(sourceTestCase.xRef,sourceTestCase.yRef,sourceTestCase.nominalSpeed);
+    load('vehiclesMIddle.mat');
+    testCase=sourceTestCase;
+    testCase.psi1=psi_1;
+    testCase.psi2=psi_2;
+    testCase.slopeX1=slope_x1;
+    testCase.slopeX2=slope_x2;
+    testCase.slopeY1=slope_y1;
+    testCase.slopeY2=slope_y2;
+    testCase.X1=x_1;
+    testCase.X2=x_2;
+    testCase.Y1=y_1;
+    testCase.Y2=y_2;
+    
+end
+
+function testCase = generateFollowUpMRIP3(sourceTestCase)
+    %MRIP: change of the waypoints from a direction to another
+    testCase = sourceTestCase;
+    testCase.xRef = flip(testCase.xRef);
+    testCase.yRef = flip(testCase.yRef);
+    testCase.refPose=flip(testCase.refPose);
+end
+
+function testCase=generateFollowUpMRIP4(sourceTestCase)
+    %MRIP: Remove 20% of the guidance points
+    testCase = sourceTestCase;
+    toDelete=randi([2,size(sourceTestCase.xRef,1)-1],round(size(sourceTestCase.xRef,1)*0.2),1);
+    for j=1: size(toDelete,1)
+        testCase.xRef(toDelete(j,1),1)=1000;
+        testCase.yRef(toDelete(j,1),1)=1000;
+    end
+    testCase.xRef=testCase.xRef(testCase.xRef~=1000);
+    testCase.yRef=testCase.yRef(testCase.yRef~=1000);
+    refPose(:,1)=testCase.xRef(:,1);
+    refPose(:,2)=testCase.yRef(:,1);
+    testCase.refPose=refPose;
+end
--- METAMORPHIC_TESTING_EXPERIMENT_MAIN.m
+++ METAMORPHIC_TESTING_EXPERIMENT_MAIN.m
@@ -8,33 +8,27 @@ addpath('functions');
 addpath('ExperimentalMutants');
 setUpPurePursuitUSCity;
 %% Enumerate MRIPs and mutants
-disp('Initializing MRIPs and Mutants...')
-MRIP={@(a) generateFollowUpMRIP1_1(a),@(a) generateFollowUpMRIP1_2(a),@(a) generateFollowUpMRIP1_3(a),@(a) generateFollowUpMRIP2(a),@(a) generateFollowUpMRIP3(a),@(a) generateFollowUpMRIP4(a)};
 MRIP_Names=["MRIP1_1","MRIP1_2","MRIP1_3","MRIP2","MRIP3", "MRIP4"];
-MRIP_Count = size(MRIP,2);
+MRIP_Count = size(MRIP_Names,2);
 Mutants=["purePursuitUSCity","Mutant_1_of_purePursuitUSCity","Mutant_2_of_purePursuitUSCity","Mutant_3_of_purePursuitUSCity","Mutant_4_of_purePursuitUSCity","Mutant_5_of_purePursuitUSCity","Mutant_6_of_purePursuitUSCity","Mutant_7_of_purePursuitUSCity","Mutant_8_of_purePursuitUSCity","Mutant_9_of_purePursuitUSCity","Mutant_10_of_purePursuitUSCity",...
     "Mutant_11_of_purePursuitUSCity","Mutant_12_of_purePursuitUSCity","Mutant_13_of_purePursuitUSCity","Mutant_14_of_purePursuitUSCity","Mutant_15_of_purePursuitUSCity","Mutant_16_of_purePursuitUSCity","Mutant_17_of_purePursuitUSCity","Mutant_18_of_purePursuitUSCity","Mutant_19_of_purePursuitUSCity","Mutant_20_of_purePursuitUSCity"];
 Mutants_Count = size(Mutants,2);
 %% Init Results table
 ResultsTableFile = 'Experiment_Results.csv';
-ResultsTableFile2 = 'Experiment_Results.xlsx';
 ResultsTableVariableNames={'Model','MRIP','Test Case','# of Waypoints','Error distance (Source)','Error distance (FollowUp)','Time to destination (Source)','Time to destination (FollowUp)','Balancing (Source)','Balancing (FollowUp)','Distance to the car Follow up','Distance to the car Source','Source exec time','Follow up exec time'};
 %ResultsTableWrittenBefore = max(fcountlines(ResultsTableFile) - 1, 0);
 %disp(['ResultsTableWrittenBefore = ' num2str(ResultsTableWrittenBefore)]);
 ResultsTableCreated = false;
 %% Init state
+load testSuite;
+nTest = size(sourceTestSuite, 2);
 if isfile("experimentProgress.mat")
     disp('Loading state from experimentProgress.mat...')
     load('experimentProgress.mat');
-    rng(rngstate);
-    nTest = size(testSuite, 2);
     ResultsTableCreated = mutant_index + mrip_index + test_index > 1;
 else
     %% Test generation
-    rng(1);
     disp('Generating source test cases...')
-    nTest = 100; %Number of test cases to generate
-    testSuite = generateTestCasesRandomly(nTest);
     mutant_index = 0;
     mrip_index = 0;
     test_index = 1;
@@ -48,107 +42,34 @@ for i=mutant_index:Mutants_Count-1
            mutant_index = i;
            mrip_index = j;
            test_index = ii;
-           rngstate = rng();
-           save('experimentProgress.mat', 'testSuite', 'rngstate', 'mutant_index', 'mrip_index', 'test_index');
+           save('experimentProgress.mat', 'mutant_index', 'mrip_index', 'test_index');
            % Display current test execution
            disp('========================================')
            disp(['Mutant = ' num2str(i+1) '/' num2str(Mutants_Count)]);
            disp(['MRIP = ' num2str(j+1) '/' num2str(MRIP_Count)]);
            disp(['Test case = ' num2str(ii) '/' num2str(nTest)])
-           %disp(['Number of Waypoints = ' num2str(size(xRef,1))]);
-           %disp(['Error distance(Source) = ' num2str(QoSMeasure.errorDistance)]); %a higher nominal speed shall have a higher error distance
-           %disp(['Error distance(FollowUP) = ' num2str(QoSMeasureFollowUp.errorDistance )]); %a higher nominal speed shall have a higher error distance
-           %disp(['Time to destination (Source) = ', num2str(QoSMeasure.timeToDestination)]); % a higher nominal speed and higher minimal speed shall have a lower time to destination
-           %disp(['Time to destination (FollowUp) = ', num2str(QoSMeasureFollowUp.timeToDestination)]); % a higher nominal speed and higher minimal speed shall have a lower time to destination   
-           %disp(['Balancing (Source) = ', num2str(QoSMeasure.balancing)]); % a higher norminal speed shall have a higher balancing || a higher bicycle length and lookahead distance shall have a higher balancing
-           %disp(['Balancing (FollowUp) = ', num2str(QoSMeasureFollowUp.balancing )]); % a higher norminal speed shall have a higher balancing || a higher bicycle length and lookahead distance shall have a higher balancing
-           %disp(['Execution time (Source) = ' num2str(testDurSource)]); 
-           %disp(['Execution time (FollowUP) = ' num2str(testDurFollowUp)]);
-           t=datetime('now');
-           disp(['Execution moment=  ' datestr(t)]);
+           disp(['Execution moment=  ' datestr(datetime('now'))]);
            % Execute source test case
            if j == 0 || exist('QoSMeasure','var') ~= 1
               tic;
-              QoSMeasure = executeTestCase(testSuite{ii},Mutants(1,i+1));
+              QoSMeasure = executeTestCase(sourceTestSuite{ii},Mutants(1,i+1));
               testDurSource = toc;
            end
            % Execute follow-up test case
            tic;
-           QoSMeasureFollowUp = executeTestCase(MRIP{j+1}(testSuite{ii}),Mutants(1,i+1));
+           QoSMeasureFollowUp = executeTestCase(followupTestSuite{i+1, j+1, ii},Mutants(1,i+1));
            testDurFollowUp = toc;
            num=ii+(nTest*j)+i*nTest*size(MRIP,2);
-           ResultsTable=table(Mutants(1,i+1),MRIP_Names(1,j+1),ii,size(testSuite{ii}.xRef,1),QoSMeasure.errorDistance,QoSMeasureFollowUp.errorDistance,QoSMeasure.timeToDestination,QoSMeasureFollowUp.timeToDestination,QoSMeasure.balancing,QoSMeasureFollowUp.balancing,QoSMeasureFollowUp.Distance,QoSMeasure.Distance,testDurSource,testDurFollowUp);   
+           ResultsTable=table(Mutants(1,i+1),MRIP_Names(1,j+1),ii,size(sourceTestSuite{ii}.xRef,1),QoSMeasure.errorDistance,QoSMeasureFollowUp.errorDistance,QoSMeasure.timeToDestination,QoSMeasureFollowUp.timeToDestination,QoSMeasure.balancing,QoSMeasureFollowUp.balancing,QoSMeasureFollowUp.Distance,QoSMeasure.Distance,testDurSource,testDurFollowUp);   
            % Write results table
            if ResultsTableCreated
-            writetable(ResultsTable, ResultsTableFile, "WriteMode", "append", "WriteVariableNames", false);
-            writetable(ResultsTable, ResultsTableFile2, "WriteMode", "append", "WriteVariableNames", false);
+                writetable(ResultsTable, ResultsTableFile, "WriteMode", "append", "WriteVariableNames", false);
            else
-            ResultsTable.Properties.VariableNames=ResultsTableVariableNames;
-            writetable(ResultsTable, ResultsTableFile);
-            writetable(ResultsTable, ResultsTableFile2);
-            ResultsTableCreated = true;
+                ResultsTable.Properties.VariableNames=ResultsTableVariableNames;
+                writetable(ResultsTable, ResultsTableFile);
+                ResultsTableCreated = true;
            end
         end
     end
     %writetable(ResultsTable,'Experiment_Results.xlsx');
-end
-
-
-function testCase = generateFollowUpMRIP1_1(sourceTestCase)
-    %MRIP: increment nominal speed
-    testCase = sourceTestCase;
-    testCase.nominalSpeed = testCase.nominalSpeed *1.1;
-    
-end
-
-function testCase = generateFollowUpMRIP1_2(sourceTestCase)
-    %MRIP: increment nominal speed
-    testCase = sourceTestCase;
-    testCase.nominalSpeed = testCase.nominalSpeed *1.2;
-end
-function testCase = generateFollowUpMRIP1_3(sourceTestCase)
-    %MRIP: increment nominal speed
-    testCase = sourceTestCase;
-    testCase.nominalSpeed = testCase.nominalSpeed *1.3;
-end
-
-function testCase = generateFollowUpMRIP2(sourceTestCase)
-    %MRIP: Generate vehicles inside of the vehicle's path
-    generateVehiclesMiddle(sourceTestCase.xRef,sourceTestCase.yRef,sourceTestCase.nominalSpeed);
-    load('vehiclesMIddle.mat');
-    testCase=sourceTestCase;
-    testCase.psi1=psi_1;
-    testCase.psi2=psi_2;
-    testCase.slopeX1=slope_x1;
-    testCase.slopeX2=slope_x2;
-    testCase.slopeY1=slope_y1;
-    testCase.slopeY2=slope_y2;
-    testCase.X1=x_1;
-    testCase.X2=x_2;
-    testCase.Y1=y_1;
-    testCase.Y2=y_2;
-    
-end
-
-function testCase = generateFollowUpMRIP3(sourceTestCase)
-    %MRIP: change of the waypoints from a direction to another
-    testCase = sourceTestCase;
-    testCase.xRef = flip(testCase.xRef);
-    testCase.yRef = flip(testCase.yRef);
-    testCase.refPose=flip(testCase.refPose);
-end
-
-function testCase=generateFollowUpMRIP4(sourceTestCase)
-    %MRIP: Remove 20% of the guidance points
-    testCase = sourceTestCase;
-    toDelete=randi([2,size(sourceTestCase.xRef,1)-1],round(size(sourceTestCase.xRef,1)*0.2),1);
-    for j=1: size(toDelete,1)
-        testCase.xRef(toDelete(j,1),1)=1000;
-        testCase.yRef(toDelete(j,1),1)=1000;
-    end
-    testCase.xRef=testCase.xRef(testCase.xRef~=1000);
-    testCase.yRef=testCase.yRef(testCase.yRef~=1000);
-    refPose(:,1)=testCase.xRef(:,1);
-    refPose(:,2)=testCase.yRef(:,1);
-    testCase.refPose=refPose;
 end
