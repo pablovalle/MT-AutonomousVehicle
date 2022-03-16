efb1ef9d7a5542bc1b2c1b93c78f007a2bd24329
Jon Ayerdi <jon_ayerdi_33@hotmail.es>
2022-03-16 15:38:58 +0100

    clean up evaluations script

--- CompileEvaluationResults.py
+++ CompileEvaluationResults.py
@@ -6,11 +6,10 @@ Created on Sat Mar 12 10:26:46 2022
 """
 
 import pandas as pd
-import math
 
 FILE_IN  = "Results\Experiment_Results.xlsx"
-FILE_OUT ='Results\EvaluationFinal_{sheet_name}.xlsx'
-#FILE_OUT ='Evaluation.xlsx'
+FILE_OUT ='Evaluation.xlsx'
+#FILE_OUT ='Results\EvaluationFinal_{sheet_name}.csv'
 
 FORMAT_IN = 'csv'
 if FILE_IN.endswith('.xlsx'):
@@ -28,26 +27,25 @@ data = data.drop(['# of Waypoints','Error distance (Source)', 'Error distance (F
                  'Distance to the car Follow up', 'Distance to the car Source','Source exec time',
                  'Follow up exec time'  ],axis=1)
 
-data['Balancing (Source)'].loc[(data['Balancing (Source)']<0.1)]=0.1
-data['Balancing (FollowUp)'].loc[(data['Balancing (FollowUp)']<0.1)]=0.1
+EPSILON = 0.1
+data['Balancing (Source)'].loc[(data['Balancing (Source)']<EPSILON)]=EPSILON
+data['Balancing (FollowUp)'].loc[(data['Balancing (FollowUp)']<EPSILON)]=EPSILON
 toDiscard_MRIP3=list()
 toDiscard_MRIP4=list()
+
 def calcDetection_Mrip1(data, mrip_to, mrip_td, threshold_td, threshold_to):
     
     if data['Time to destination (Source)']*threshold_td<=data['Time to destination (FollowUp)']:
         mrip_td=mrip_td+1
     if data['Balancing (Source)']>=data['Balancing (FollowUp)']*threshold_to:
         mrip_to=mrip_to+1
-    
     return mrip_to, mrip_td
 
 def calcDetection_Mrip2(data, mrip_to, mrip_td, threshold_td, threshold_to):
     if data['Time to destination (Source)']>=data['Time to destination (FollowUp)']*threshold_td:
         mrip_td=mrip_td+1
-
     if abs(data['Balancing (Source)']-data['Balancing (FollowUp)'])/data['Balancing (Source)']>=threshold_to:
         mrip_to=mrip_to+1
-    
     return mrip_to, mrip_td
 
 def calcDetection_Mrip3(data, mrip_to, mrip_td, threshold_td, threshold_to):
@@ -58,8 +56,6 @@ def calcDetection_Mrip3(data, mrip_to, mrip_td, threshold_td, threshold_to):
         mrip_to=mrip_to+1
         if data['Model']==caseStudies[0]:
             toDiscard_MRIP3.append(data['Test Case'])
-        
-    
     return mrip_to, mrip_td
 
 def calcDetection_Mrip4(data, mrip_to, mrip_td, threshold_td, threshold_to):
@@ -69,15 +65,6 @@ def calcDetection_Mrip4(data, mrip_to, mrip_td, threshold_td, threshold_to):
         mrip_to=mrip_to+1
         if data['Model']==caseStudies[0]:
             toDiscard_MRIP4.append(data['Test Case'])
-    
-    return mrip_to, mrip_td
-def compareWithOriginal(data1, data2, mrip_to, mrip_td):
-
-    if data1['Time to destination (Source)']<data2['Time to destination (Source)']:
-        mrip_td=mrip_td+1
-    if data1['Balancing (Source)']<data2['Balancing (Source)']:
-        mrip_to=mrip_to+1
-    
     return mrip_to, mrip_td
 
 print("Data loaded")
@@ -94,10 +81,7 @@ for i in range(0,nTest):
             
 results_To=pd.DataFrame({"Mutant":[], "MRIP1_1":[], "MRIP1_2":[], "MRIP1_3":[], "MRIP2":[], "MRIP3":[], "MRIP4":[]})
 results_Td=pd.DataFrame({"Mutant":[], "MRIP1_1":[], "MRIP1_2":[], "MRIP1_3":[], "MRIP2":[], "MRIP3":[], "MRIP4":[]})
-results_ToPos=pd.DataFrame({"Mutant":[], "MRIP1_1":[], "MRIP1_2":[], "MRIP1_3":[], "MRIP2":[], "MRIP3":[], "MRIP4":[]})
-results_TdPos=pd.DataFrame({"Mutant":[], "MRIP1_1":[], "MRIP1_2":[], "MRIP1_3":[], "MRIP2":[], "MRIP3":[], "MRIP4":[]})
-  
-            
+
 for i in range(0,nStudies):
     mrip1_1_to=0
     mrip1_2_to=0
@@ -112,20 +96,7 @@ for i in range(0,nStudies):
     mrip2_td=0
     mrip3_td=0
     mrip4_td=0
-    
-    mrip1_1_toPos=0
-    mrip1_2_toPos=0
-    mrip1_3_toPos=0
-    mrip2_toPos=0
-    mrip3_toPos=0
-    mrip4_toPos=0
-    
-    mrip1_1_tdPos=0
-    mrip1_2_tdPos=0
-    mrip1_3_tdPos=0
-    mrip2_tdPos=0
-    mrip3_tdPos=0
-    mrip4_tdPos=0
+
     print("Running on mutant: "+caseStudies[i])
     for j in range(0,nMrip):
         for k in range(0, nTest):
@@ -135,53 +106,29 @@ for i in range(0,nStudies):
             indice2=j*nTest+k
             if data['MRIP'][indice]=="MRIP1_1":
                 mrip1_1_to, mrip1_1_td=calcDetection_Mrip1(data.iloc[indice],mrip1_1_to,mrip1_1_td,1.1,1.3)
-                if i>0:
-                   mrip1_1_toPos, mrip1_1_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip1_1_toPos,mrip1_1_tdPos)                 
+
             elif data['MRIP'][indice]=="MRIP1_2":
                 mrip1_2_to, mrip1_2_td=calcDetection_Mrip1(data.iloc[indice],mrip1_2_to,mrip1_2_td,1.1,1.3)
-                if i>0:
-                   mrip1_2_toPos, mrip1_2_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip1_2_toPos,mrip1_2_tdPos)                 
-            
+
             elif data['MRIP'][indice]=="MRIP1_3":
                 mrip1_3_to, mrip1_3_td=calcDetection_Mrip1(data.iloc[indice],mrip1_3_to,mrip1_3_td,1.1,1.3)
-                if i>0:
-                   mrip1_3_toPos, mrip1_3_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip1_3_toPos,mrip1_3_tdPos)                 
-            
+
             elif data['MRIP'][indice]=="MRIP2":
                 mrip2_to, mrip2_td=calcDetection_Mrip2(data.iloc[indice],mrip2_to,mrip2_td,1.1,0.3)
-                if i>0:
-                   mrip2_toPos, mrip2_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip2_toPos,mrip2_tdPos)                 
-            
+
             elif data['MRIP'][indice]=="MRIP3":
                 mrip3_to, mrip3_td=calcDetection_Mrip3(data.iloc[indice],mrip3_to,mrip3_td,0.15,0.3)
-                if i>0:
-                   mrip3_toPos, mrip3_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip3_toPos,mrip3_tdPos)                 
-            
+
             elif data['MRIP'][indice]=="MRIP4":
                 mrip4_to, mrip4_td=calcDetection_Mrip4(data.iloc[indice],mrip4_to,mrip4_td,0.1,0.3)
-                if i>0:
-                   mrip4_toPos, mrip4_tdPos=compareWithOriginal(data.iloc[indice],data.iloc[indice2],mrip4_toPos,mrip4_tdPos)                 
-            
-    
-    
+
     results_To=results_To.append({"Mutant":caseStudies[i], "MRIP1_1":mrip1_1_to, "MRIP1_2":mrip1_2_to, "MRIP1_3":mrip1_3_to, "MRIP2":mrip2_to, "MRIP3":mrip3_to, "MRIP4":mrip4_to}, ignore_index=True)
     results_Td=results_Td.append({"Mutant":caseStudies[i], "MRIP1_1":mrip1_1_td, "MRIP1_2":mrip1_2_td, "MRIP1_3":mrip1_3_td, "MRIP2":mrip2_td, "MRIP3":mrip3_td, "MRIP4":mrip4_td}, ignore_index=True)
-    results_ToPos=results_ToPos.append({"Mutant":caseStudies[i], "MRIP1_1":mrip1_1_toPos, "MRIP1_2":mrip1_2_toPos, "MRIP1_3":mrip1_3_toPos, "MRIP2":mrip2_toPos, "MRIP3":mrip3_toPos, "MRIP4":mrip4_toPos}, ignore_index=True)
-    results_TdPos=results_TdPos.append({"Mutant":caseStudies[i], "MRIP1_1":mrip1_1_tdPos, "MRIP1_2":mrip1_2_tdPos, "MRIP1_3":mrip1_3_tdPos, "MRIP2":mrip2_tdPos, "MRIP3":mrip3_tdPos, "MRIP4":mrip4_tdPos}, ignore_index=True)
 
-results_To.to_excel(FILE_OUT, sheet_name="FAILURES_TO", index=False, header=False)
-with pd.ExcelWriter(FILE_OUT,engine="openpyxl" ,mode='a') as writer: 
-    results_Td.to_excel(writer, sheet_name="FAILURES_TD", index=False, header=False)
-    results_ToPos.to_excel(writer, sheet_name="POSSIBLE_FAILURES_TO", index=False, header=False)
-    results_TdPos.to_excel(writer, sheet_name="POSSIBLE_FAILURES_TD", index=False, header=False)      
-
-
-
-
-
-
-
-
-
-
-
+if FORMAT_OUT == 'xlsx':
+    results_To.to_excel(FILE_OUT, sheet_name="FAILURES_TO", index=False, header=False)
+    with pd.ExcelWriter(FILE_OUT,engine="openpyxl" ,mode='a') as writer: 
+        results_Td.to_excel(writer, sheet_name="FAILURES_TD", index=False, header=False)  
+else:
+    results_To.to_csv(FILE_OUT.format(sheet_name='FAILURES_TO'), index=False, header=False)
+    results_Td.to_csv(FILE_OUT.format(sheet_name='FAILURES_TD'), index=False, header=False)
